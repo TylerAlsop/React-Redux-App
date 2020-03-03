b0ae27edaef313a6eb6f2b8c3c73e6354c927fed
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-02 19:43:12 -0700

    Created components and store folders. In the store folder created actions and reducers folders each with an index.js. Added some potential API that interested me to store/actions/index.js and where to find them.

---
+++ async-redux-api-fetch/src/store/actions/index.js
@@ -0,0 +1,10 @@
+// Space X:
+// https://api.spacexdata.com/v3/launches/latest
+// found at: https://github.com/r-spacex/SpaceX-API
+
+// Launch Library:
+// https://launchlibrary.net/1.3/agency
+// found at: https://launchlibrary.net/docs/1.3/api.html
+
+// this list of animal apis:
+// https://github.com/public-apis/public-apis#animals
