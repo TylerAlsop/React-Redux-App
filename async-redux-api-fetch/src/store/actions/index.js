0b621a0f27a171a0e685c2d152d9c32b332b4cf2
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-04 08:58:22 -0700

    Setup basic actions for fetching start, success, and failiure in actions/index.js.

--- async-redux-api-fetch/src/store/actions/index.js
+++ async-redux-api-fetch/src/store/actions/index.js
@@ -1,3 +1,34 @@
+export const FETCHING_START = 'FETCHING_START';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+export const fetchingStart = item => {
+    console.log("fetchingStart from actions", item);
+    return {
+      type: "FETCHING_START",
+      payload: item
+    };
+};
+
+export const fetchingSuccess = item => {
+    console.log("fetchingSuccess from actions", item);
+    return {
+      type: "FETCHING_SUCCESS",
+      payload: item
+    };
+};
+
+export const fetchingFailure = item => {
+    console.log("fetchingFailure from actions", item);
+    return {
+      type: "FETCHING_FAILURE",
+      payload: item
+    };
+};
+
+
+
+
 // Space X:
 // https://api.spacexdata.com/v3/launches/latest
 // found at: https://github.com/r-spacex/SpaceX-API
