189ddc38086884e775d41844d295401fd273212f
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-03 19:46:36 -0700

    Created a blank initial state and blank reducer in store/reducers/index.js. Imported reducer, Provider, createStore, and applyMiddleware into index.js and wrapped <App> in <Provider>.

---
+++ async-redux-api-fetch/src/store/reducers/index.js
@@ -0,0 +1,10 @@
+export const initialState = {}
+
+export const reducer = (state = initialState, action) => {
+    console.log("State and Action(s) in featureReducer", state, action);
+    switch(action.type) {
+        case "":
+            return {}
+        default state;
+    }
+}
